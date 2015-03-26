458e58338b75c42cc4cb40ba64a0f9ac08175081
jennifer <jennifer@owncloud.com>
2015-03-26 10:27:08 -0400

    change check for use of ldap users

--- lib/oc-tests/test_shareFile.py
+++ lib/oc-tests/test_shareFile.py
@@ -59,6 +59,7 @@ Data Providers:
 from smashbox.utilities import *
 import glob
 import time
+import sys
 
 OCS_PERMISSION_READ = 1
 OCS_PERMISSION_UPDATE = 2
@@ -91,6 +92,7 @@ def setup(step):
 
     reset_rundir()
 
+
 @add_worker
 def sharer(step):
 
@@ -113,7 +115,7 @@ def sharer(step):
 
     step (4,'Sharer shares files')
 
-    if config.use_ldap_users:
+    if config.ldap_usernames:
         user1 = config.ldap_usernames[0]
         user2 = config.ldap_usernames[1]
     else:
@@ -180,7 +182,7 @@ def shareeOne(step):
 
     step (8, 'Sharee One share files with user 3')
 
-    if config.use_ldap_users:
+    if config.ldap_usernames:
         user2 = config.ldap_usernames[1]
         user3 = config.ldap_usernames[2]
     else:
