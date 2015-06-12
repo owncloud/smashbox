525c01868232b62c288a996370c1488b30057865
Juan Pablo Villafáñez <jvillafanez@solidgear.es>
2015-06-12 13:55:51 +0200

    Retry action after the download to check that the lock is released

--- lib/oc-tests/new_locking/test_downloadFilePlusAction.py
+++ lib/oc-tests/new_locking/test_downloadFilePlusAction.py
@@ -83,49 +83,49 @@ def parse_worker_number(worker_name):
 testsets = [
         { 'action_method': 'put_file_contents',
           'action_args': ('/folder/bigfile.dat', '123'*50),
-          'action_kwargs': {'pyocactiondebug' : True},
+          'action_kwargs': {},
           'accounts': sconf.oc_number_test_users,
           'extra_check': 'check_filesize',
           'extra_check_params': ('/folder/bigfile.dat', 3*50)
         },
         { 'action_method': 'delete',
           'action_args': ('/folder/bigfile.dat',),
-          'action_kwargs': {'pyocactiondebug' : True},
+          'action_kwargs': {},
           'accounts': sconf.oc_number_test_users,
           'extra_check': 'check_file_not_exists',
           'extra_check_params': ('/folder/bigfile.dat',)
         },
         { 'action_method': 'file_info',
           'action_args': ('/folder/bigfile.dat',),
-          'action_kwargs': {'pyocactiondebug' : True},
+          'action_kwargs': {},
           'accounts': sconf.oc_number_test_users,
           'extra_check': None,
           'extra_check_params': ()
         },
         { 'action_method': 'move',
           'action_args': ('/folder/bigfile.dat', '/folder/bigrenamed.dat'),
-          'action_kwargs': {'pyocactiondebug' : True},
+          'action_kwargs': {},
           'accounts': sconf.oc_number_test_users,
           'extra_check': 'check_first_exists_second_not',
           'extra_check_params': ('/folder/bigrenamed.dat', '/folder/bigfile.dat')
         },
         { 'action_method': 'move',
           'action_args': ('/folder/bigfile.dat', '/folder2/bigfile.dat'),
-          'action_kwargs': {'pyocactiondebug' : True},
+          'action_kwargs': {},
           'accounts': sconf.oc_number_test_users,
           'extra_check': 'check_first_exists_second_not',
           'extra_check_params': ('/folder2/bigfile.dat', '/folder/bigfile.dat')
         },
         { 'action_method': 'delete',
           'action_args': ('/folder',),
-          'action_kwargs': {'pyocactiondebug' : True},
+          'action_kwargs': {},
           'accounts': sconf.oc_number_test_users,
           'extra_check': 'check_all_files_not_exists',
           'extra_check_params': ('/folder/bigfile.dat', '/folder')
         },
         { 'action_method': 'move',
           'action_args': ('/folder', '/folder-renamed'),
-          'action_kwargs': {'pyocactiondebug' : True},
+          'action_kwargs': {},
           'accounts': sconf.oc_number_test_users,
           'extra_check': 'check_first_list_exists_second_list_not',
           'extra_check_params': (('/folder-renamed', '/folder-renamed/bigfile.dat'),
@@ -133,7 +133,7 @@ testsets = [
         },
         { 'action_method': 'move',
           'action_args': ('/folder', '/folder2/folder'),
-          'action_kwargs': {'pyocactiondebug' : True},
+          'action_kwargs': {},
           'accounts': sconf.oc_number_test_users,
           'extra_check': 'check_first_list_exists_second_list_not',
           'extra_check_params': (('/folder2', '/folder2/folder', '/folder2/folder/bigfile.dat'),
@@ -196,25 +196,29 @@ def doer(step):
 
     step(4, 'action over file')
 
+    retry_action = False
     # perform the action
     try:
         result = pyocaction(user_account, sconf.oc_account_password, False, method, *args, **kwargs)
 
-        step(4, 'check results')
-        # check successful result
-        logger.debug('check %s method finished correctly' % method)
-        error_check(result, method + ' action didn\'t finish correctly')
-
-        # perform extra check
-        check = config.get('extra_check', None)
-        if check:
-            logger.debug('additional check %s' % check)
-            check_params = config.get('extra_check_params', ())
-            error_check(globals()[check](*check_params), 'extra check failed: %s %s' % (check, check_params))
     except owncloud.ResponseError as e:
         logger.debug('%s action failed. Checking the status to know if the file is locked' % (method,))
         error_check(e.status_code == 423, 'unexpected status code [%i] : %s' % (e.status_code, e.get_resource_body()))
+        retry_action = True
 
+    step(6, 'check results')
+    if retry_action:
+        result = pyocaction(user_account, sconf.oc_account_password, False, method, *args, **kwargs)
+    # check successful result
+    logger.debug('check %s method finished correctly' % method)
+    error_check(result, method + ' action didn\'t finish correctly')
+
+    # perform extra check
+    check = config.get('extra_check', None)
+    if check:
+        logger.debug('additional check %s' % check)
+        check_params = config.get('extra_check_params', ())
+        error_check(globals()[check](*check_params), 'extra check failed: %s %s' % (check, check_params))
 
 # add workers
 for i in range(config.get('accounts', 1)):
