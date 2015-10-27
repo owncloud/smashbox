f46c38fb61f752ce231b63726e1d3766ba57fd34
Joas Schilling <nickvergessen@owncloud.com>
2015-10-27 10:21:30 +0100

    Make sure that moving a non-existing file returns a 404

---
+++ lib/owncloud/test_moveFileStatusCode.py
@@ -0,0 +1,47 @@
+from owncloud import HTTPResponseError
+
+__doc__ = """
+
+Test moving a file via webdav
+
+"""
+
+from smashbox.utilities import *
+
+@add_worker
+def move_non_existing_file(step):
+
+    step(1, 'Create a folder and a file')
+    d = make_workdir()
+    dir_name = os.path.join(d, 'folder')
+    local_dir = make_workdir(dir_name)
+
+    createfile(os.path.join(d, 'file1.txt'), '0', count=1000, bs=50)
+    createfile(os.path.join(local_dir, 'file2.txt'), '1', count=1000, bs=50)
+    run_ocsync(d, user_num=1)
+
+    expect_webdav_exist('file1.txt', user_num=1)
+    expect_webdav_exist(os.path.join('folder', 'file2.txt'), user_num=1)
+
+    step(2, 'Move the file into the folder')
+
+    api = get_oc_api()
+    api.login("%s%i" % (config.oc_account_name, 1), config.oc_account_password)
+
+    try:
+        api.move('file1.txt', os.path.join('folder', 'file2.txt'))
+    except HTTPResponseError as err:
+        error_check(
+            False,
+            'Server replied with status code: %i' % err.status_code
+        )
+
+    step(3, 'Move non existing file into the folder')
+
+    try:
+        api.move('file1.txt', os.path.join('folder', 'file2.txt'))
+    except HTTPResponseError as err:
+        error_check(
+            err.status_code == 404,
+            'Server replied with status code: %i' % err.status_code
+        )
